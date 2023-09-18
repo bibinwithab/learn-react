09b25e3a3c09228bad193e38dd076a15e47d22b0
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2023-09-18 19:09:15 +0530

    conditional rendering

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,12 +1,15 @@
 function ListGroup() {
-  const items = ["New Delhi", "Tokyo", "Saint Lopez", "L.A."];
+  var items = ["New Delhi", "Tokyo", "Saint Lopez", "L.A.", "California"];
 
   return (
     <>
       <h1>Bucket List</h1>
+      {items.length == 0 && <p>No Values Found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item">{item}</li>
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
