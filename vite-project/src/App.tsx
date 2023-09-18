31b85080857aa7c0e6cd50ea70d439d5cc34eece
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2023-09-18 21:03:08 +0530

    prop fn

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,6 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-
   let items = [
     "New Delhi",
     "Tokyo",
@@ -10,8 +9,20 @@ function App() {
     "California",
     "Boston",
   ];
-  
-  return <div><ListGroup items={items} heading="Cities"/></div>;
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
